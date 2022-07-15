12042883f1799a3131413bb58d1a914f7092198c
kodaishiotsuki <0711dr5@gmail.com>
2022-07-15 17:59:42 +0900

    donegit add .

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/CartSlice";
+import { closeModal } from "../features/modal/ModalSlice";
 
 const Modal = () => {
+  const dispatch = useDispatch();
   return (
     <aside className="modal-container">
       <div className="modal">
         <h4>買い物かごを全て空にしますか??</h4>
         <div className="btn-container">
-          <button className="btn confirm-btn">OK</button>
-          <button className="btn clear-btn">やめとく</button>
+          <button
+            className="btn confirm-btn"
+            onClick={() => {
+              dispatch(clearCart()); //全削除
+              dispatch(closeModal()); //モーダル閉じる
+            }}
+          >
+            OK
+          </button>
+          <button
+            className="btn clear-btn"
+            onClick={() => dispatch(closeModal())}
+          >
+            やめとく
+          </button>
         </div>
       </div>
     </aside>
