2f0efcd1f88ad3827e5c2da07fd30e6e298cc110
kodaishiotsuki <0711dr5@gmail.com>
2022-07-15 16:04:06 +0900

    1つの商品を削除するReducerを作成

--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -20,9 +20,14 @@ const cartSlice = createSlice({
       // state.total = 0;
       return { cartItems: [], amount: 0, total: 0 };
     },
+    removeItem: (state, action) => {
+      // console.log(action);//actionはtype,payloadを含む
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId); //選択したid以外を残す(選択したid削除)
+    },
   },
 });
 // console.log(cartSlice);
 
-export const { clearCart } = cartSlice.actions; //console.log(cartSlice)参照
+export const { clearCart, removeItem } = cartSlice.actions; //console.log(cartSlice)参照
 export default cartSlice.reducer;
