12e56fb4857874ecd84628b2d0e7253a8aeed29f
kodaishiotsuki <0711dr5@gmail.com>
2022-07-15 17:47:00 +0900

    ModalSlice作成 && 全削除を押してモーダル出現

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
 import { calculateTotals } from "./features/cart/CartSlice";
 
 function App() {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
 
   //cartItemsの中の状態が変わったタイミングで発火!!
   useEffect(() => {
@@ -16,6 +18,7 @@ function App() {
 
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/CartSlice";
+import modalReducer from "./features/modal/ModalSlice";
 
 //storeの中にreducerを作る
+//sliceを作成時に忘れず定義!!
 export const store = configureStore({
   reducer: {
     cart: cartReducer, //reducer
+    modal: modalReducer,
   },
 });
